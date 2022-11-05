d14555c66167ae8651b74ca8784ff51f77c12c06
wobsoriano <sorianorobertc@gmail.com>
2022-11-04 22:58:41 -0700

    update types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 export {
   styled as default,
 } from './styled'
-
-export type {
-  VariantsToProps,
-} from './styled'
--- src/styled.ts
+++ src/styled.ts
@@ -1,72 +1,43 @@
 import type * as Vue from 'vue'
 import type { RuntimeFn, VariantGroups } from '@vanilla-extract/recipes/dist/declarations/src/types'
 import { computed, defineComponent, h } from 'vue'
-import type {
-  RecipeVariants,
-} from '@vanilla-extract/recipes'
 import type { IntrinsicElementAttributes } from './element-types'
-
-type PublicProps = Vue.VNodeProps & Vue.AllowedComponentProps & Vue.ComponentCustomProps
 
 type IntrinsicElementsKeys = keyof IntrinsicElementAttributes
 
-type NonUndefinedable<T> = T extends undefined ? never : T
-type TypePropsToRuntimeProps<T> = {
-  [K in keyof T]-?: {} extends Pick<T, K> ? {
-    type: Vue.PropType<NonUndefinedable<T[K]>>
-  } : {
-    type: Vue.PropType<T[K]>
-    required: true
-  };
-}
+type Component = Vue.Component | Vue.DefineComponent
 
-export type VariantsToProps<R extends RuntimeFn<VariantGroups>> = NonNullable<RecipeVariants<R>>
+type ExtractComponentProps<TComponent> =
+  TComponent extends new () => {
+    $props: infer P
+  }
+    ? P
+    : never
 
 export function styled<
   RecipeFn extends RuntimeFn<VariantGroups>,
-  Type extends IntrinsicElementsKeys | Vue.Component,
-  PropsOrPropOptions = {},
-  RawBindings = {},
-  D = {},
-  C extends Vue.ComputedOptions = Vue.ComputedOptions,
-  M extends Vue.MethodOptions = Vue.MethodOptions,
-  Mixin extends Vue.ComponentOptionsMixin = Vue.ComponentOptionsMixin,
-  Extends extends Vue.ComponentOptionsMixin = Vue.ComponentOptionsMixin,
-  E extends Vue.EmitsOptions = Record<string, any>,
-  EE extends string = string,
-  PP = PublicProps,
-  Defaults = Vue.ExtractDefaultPropTypes<PropsOrPropOptions>,
+  Type extends IntrinsicElementsKeys | Component,
 >(
   tag: Type,
-  recipeFn: RecipeFn
-): Vue.DefineComponent<
-  TypePropsToRuntimeProps<VariantsToProps<typeof recipeFn>>,
-  RawBindings,
-  D,
-  C,
-  M,
-  Mixin,
-  Extends,
-  E,
-  EE,
-  PP,
-  Readonly<Vue.ExtractPropTypes<TypePropsToRuntimeProps<VariantsToProps<typeof recipeFn>>> &
-  (Type extends IntrinsicElementsKeys
-    ? IntrinsicElementAttributes[Type]
-    : {})>,
-  Defaults
->
-
-export function styled(
-  tag: any,
-  recipeFn: any,
+  recipeFn: RecipeFn,
 ) {
-  return defineComponent({
-    setup(_props, { attrs, slots }) {
-      const classNames = computed(() => recipeFn(JSON.parse(JSON.stringify(attrs))))
-      return () => h(tag, {
-        class: classNames.value,
-      }, slots)
-    },
+  type VariantProps = Parameters<RecipeFn>[0]
+  type ElementProps = Type extends IntrinsicElementsKeys ? IntrinsicElementAttributes[Type]
+    : Type extends Component ? ExtractComponentProps<typeof tag>
+      : never
+
+  const wrapper = defineComponent((props: Readonly<VariantProps & ElementProps>, { slots }) => {
+    const classNames = computed(() => recipeFn(props as any))
+    return () => h(tag as any, {
+      class: classNames.value,
+    }, slots)
   })
+
+  return wrapper as typeof wrapper & {
+    new (): {
+      $slots: {
+        default: (arg: VariantProps) => Vue.VNode[]
+      }
+    }
+  }
 }
